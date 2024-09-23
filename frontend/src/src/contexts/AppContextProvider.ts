d5ec095d991d976d46e0cb7cea061655a7f0f72c
hotbrainy <brainhg95@gmail.com>
2024-09-23 18:07:56 +0300

    removed -- next.js ui

--- frontend/src/src/contexts/AppContextProvider.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-interface AppContextType {
-  value: string;
-  setValue: (value: string) => void;
-}
-
-const AppContext = createContext<AppContextType | undefined>(undefined);
-export default AppContext;
--- frontend/src/src/contexts/LayoutContextProvider.ts
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-interface LayoutContextType {
-  collapsed: boolean;
-  setCollapsed: (value: boolean) => void;
-}
-
-const LayoutContext = createContext<LayoutContextType | undefined>(undefined);
-export default LayoutContext;
